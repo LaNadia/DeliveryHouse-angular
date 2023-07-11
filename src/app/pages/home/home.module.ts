e95fdc883aff0ff2b30cac0560f89b4a45e9a6ae
LaNadia <setsias@gmail.com>
2023-07-11 23:38:46 +0400

    feat: add footer ui

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,10 +4,11 @@ import {SwiperComponent} from 'src/app/components/swiper/swiper.component';
 import {AutoSliderModule} from 'src/app/shared/auto-slider/auto-slider.module';
 import {ReviewsModule} from 'src/app/shared/reviews/reviews.module';
 import {HomeComponent} from './home.component';
+import {FooterModule} from '../../components/footer/footer.module';
 
 @NgModule({
     declarations: [HomeComponent, SwiperComponent],
-    imports: [CommonModule, AutoSliderModule, ReviewsModule],
+    imports: [CommonModule, AutoSliderModule, ReviewsModule, FooterModule],
     exports: [HomeComponent],
 })
 export class HomeModule {}
